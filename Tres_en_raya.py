ea34ee2600c03e93f619577ba43a0a51c6c78652
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2023-12-01 13:09:38 +0100

    Update Tres_en_raya.py

--- Tres_en_raya.py
+++ Tres_en_raya.py
@@ -8,7 +8,6 @@ clear()
 
 partida = fr.Jugar()
 
-#esquema = np.array([["1","2","3"],["4","5","6"],["7","8","9"]])
 esquema = np.array(["-","-","-","-","-","-","-","-","-"])
 posibilidad = ["si", "true", "verdad", "vamos", "ok"]
 
